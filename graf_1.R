4630775b826ab1587f7813c96543ae01717c382e
Daniel Felipe Cendales G <dfcendalesgo@unal.edu.co>
2023-04-11 02:32:17 -0500

    Acabandooo

--- graf_1.R
+++ graf_1.R
@@ -1,8 +1,12 @@
+library(plotly)
+library(roahd)
+
 # Datos de la figura 1.1
 t <- seq(from = 0, to = 40, length = 100)
 t1 <- t/diff(range(t))
 m <- length(t)
 
+## Versión 1
 muestras <- vector(mode = 'list', length = 42)
 for(k in seq(2, 40, by = 2)){
     muestras[[k]] <- rbind(0.5*rep(sin(k*pi/10), each = m),
@@ -14,6 +18,33 @@ muestras[[41]] <- rbind(rep(0, m),
                         rep(0, m))
 muestras[[42]] <- rbind(0.5*sin(t*pi/10), 
                         0.5*cos(t*pi/10))
+
+## Versión 2
+m2 <- vector(mode = 'list', length = 2)
+t(sapply(muestras, FUN = function(k) k[1, ])) -> m2[[1]]
+t(sapply(muestras, FUN = function(k) k[2, ])) -> m2[[2]]
+
+## datos 3D
+d2 <- data.frame(x = c(t(m2[[1]])),
+                 y = c(t(m2[[2]])),
+                 t = rep(t1, times = 42),
+                 ind = rep(paste0('m', 1:42), each = 100))
+
+graf2 <- plot_ly(d2, x = ~x, y = ~y, z = ~t, split = ~ind,
+                 type = 'scatter3d', mode = 'lines',
+                 line = list(width = ~rep(c(1, 4), c(41, 1)), 
+                        #color = rep(colores, c(100, 1, 1)),
+                             lty = 1),
+                 showlegend = FALSE) %>%
+layout(scene = list(xaxis = list(range = c(-2, 2), title = ''),
+                    yaxis = list(range = c(-2, 2), title = '')))
+graf2
+
+multi <- mfData(grid = t1, Data_list = m2)
+plot(multi)
+fbplot(Data = multi, adjust = TRUE, main = 'Boxplot Funcional')
+
+
 
 # Gráfico plano
 colores <- c(rep(c('gray', 'brown'), times = 20), 'gray', 'blue')
@@ -31,4 +62,4 @@ f1 <- plot_ly(x = ~muestras[[42]][1, ], y = ~muestras[[42]][2, ], z = ~t1,
 
 f1
 
-install.packages(c('mrfDepth'))
+
